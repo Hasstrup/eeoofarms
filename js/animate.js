35b1572b8bc0733ed109e7294752b1664c10ea7b
Hasstrup Ezekiel <hasstrup.ezekiel@gmail.com>
2019-05-23 06:27:53 +0100

    mostly modifications on menu ting

--- js/animate.js
+++ js/animate.js
@@ -1,9 +1,11 @@
-var sliderTrigger = document.getElementsByClassName("slider-trigger")[0];
+var sliderTrigger = document.getElementsByClassName("slider-trigger")
 var slider = document.getElementsByClassName('slider-parent')[0];
-sliderTrigger.addEventListener( "click" , function(el){
-if(slider.classList.contains("active")){
-  slider.classList.remove("active");
- }else{
-  slider.classList.add("active");
- }
-});
+Array.from(sliderTrigger).forEach((node) => {
+ node.addEventListener( "click" , function(el){
+    if(slider.classList.contains("active")){
+      slider.classList.remove("active");
+     }else{
+      slider.classList.add("active");
+     }
+    });
+})
